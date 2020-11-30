f64f13a8556028ecdb15186121a5111bbb91b038
Orlando Eduardo Pereira <orlando.eduardo101@gmail.com>
2020-11-29 22:40:42 -0300

    features/enviarResumo #1: fix warn

--- front_end/ufrb_eventos/lib/app/modules/user_commands/domain/entities/user_entity.dart
+++ front_end/ufrb_eventos/lib/app/modules/user_commands/domain/entities/user_entity.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-
 import 'package:ufrb_eventos/app/modules/user_commands/domain/entities/inscription_entity.dart';
-import 'package:ufrb_eventos/app/modules/user_commands/domain/entities/summary_entity.dart';
 import 'package:ufrb_eventos/app/modules/user_commands/infra/model/summary_model.dart';
 
 class UserEntity {
